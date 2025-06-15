ba02b54ad693237f3f14ece7747d69904e66c3f9
WH <lwh940824@gmail.com>
2025-06-15 16:34:55 +0900

    줄맞춤

--- src/main/java/com/moeum/moeum/api/ledger/category/dto/CategoryUpdateRequestDto.java
+++ src/main/java/com/moeum/moeum/api/ledger/category/dto/CategoryUpdateRequestDto.java
@@ -9,7 +9,8 @@ import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 public record CategoryUpdateRequestDto(
-        @NotNull Long categoryGroupId,
+        @NotNull
+        Long categoryGroupId,
         @NotBlank(message = "카테고리 이름은 필수 입력입니다.")
         @Size(max = 10, message = "카테고리 이름은 10자 이내입니다.")
         String name,
