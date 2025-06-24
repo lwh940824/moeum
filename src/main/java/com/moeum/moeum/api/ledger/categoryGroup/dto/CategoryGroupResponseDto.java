c830cbe97df9def6923fe60c53ac120e49eaab07
WH <lwh940824@gmail.com>
2025-06-25 00:25:49 +0900

    record @Builder 삭제 > 불변 + 생성자 기반 매핑이라 필요없음

--- src/main/java/com/moeum/moeum/api/ledger/categoryGroup/dto/CategoryGroupResponseDto.java
+++ src/main/java/com/moeum/moeum/api/ledger/categoryGroup/dto/CategoryGroupResponseDto.java
@@ -3,9 +3,8 @@ package com.moeum.moeum.api.ledger.categoryGroup.dto;
 import com.moeum.moeum.type.CategoryType;
 import lombok.Builder;
 
-@Builder
 public record CategoryGroupResponseDto(
-        Long categoryGroupId,
+        Long id,
         String name,
         CategoryType categoryType,
         String imageUrl
