8d4df1ecd4540994eb468b1fa7689bd8131504ce
whlee <whlee@pluxity.com>
2025-06-24 11:42:51 +0900

    BaseEntity CreatedBy, LastModifiedBy 추가

--- src/main/java/com/moeum/moeum/domain/BaseEntity.java
+++ src/main/java/com/moeum/moeum/domain/BaseEntity.java
@@ -3,7 +3,9 @@ package com.moeum.moeum.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -15,9 +17,12 @@ public class BaseEntity {
     @CreatedDate
     private LocalDateTime regDt;
 
+    @CreatedBy
     private String regUser;
 
     @LastModifiedDate
     private LocalDateTime modDt;
+
+    @LastModifiedBy
     private String modUser;
 }
