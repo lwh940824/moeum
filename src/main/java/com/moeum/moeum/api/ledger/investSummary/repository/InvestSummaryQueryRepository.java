73ee388a80902a712365437101f4ec79fc329063
WH <lwh940824@gmail.com>
2025-08-30 00:27:27 +0900

    InvestSummary QueryDSL 구성

---
+++ src/main/java/com/moeum/moeum/api/ledger/investSummary/repository/InvestSummaryQueryRepository.java
@@ -0,0 +1,9 @@
+package com.moeum.moeum.api.ledger.investSummary.repository;
+
+import com.moeum.moeum.domain.InvestSummary;
+
+public interface InvestSummaryQueryRepository {
+    Long incrementPrincipal(Long investSettingId, Integer year, Integer month, Long principal);
+
+    InvestSummary upsertInvestSummary(Long investSettingId, Integer year, Integer month, Long principal);
+}
---
+++ src/main/java/com/moeum/moeum/api/ledger/investSummary/repository/InvestSummaryQueryRepositoryImpl.java
@@ -0,0 +1,69 @@
+package com.moeum.moeum.api.ledger.investSummary.repository;
+
+import com.moeum.moeum.domain.InvestSetting;
+import com.moeum.moeum.domain.InvestSummary;
+import com.moeum.moeum.domain.QInvestSummary;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class InvestSummaryQueryRepositoryImpl implements InvestSummaryQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+    private final EntityManager em;
+    private final QInvestSummary investSummary = QInvestSummary.investSummary;
+
+    @Override
+    public Long incrementPrincipal(Long investSettingId, Integer year, Integer month, Long principal) {
+        return queryFactory
+                .update(investSummary)
+                .set(investSummary.principal, investSummary.principal.add(principal))
+                .where(
+                        investSummary.investSetting.id.eq(investSettingId),
+                        investSummary.year.eq(year),
+                        investSummary.month.eq(month)
+                ).execute();
+    }
+
+    @Override
+    public InvestSummary upsertInvestSummary(Long investSettingId, Integer year, Integer month, Long principal) {
+        if (incrementPrincipal(investSettingId, year, month, principal) > 0) {
+            return selectInvestSummary(investSettingId, year, month);
+        }
+
+        try {
+            InvestSummary createInvestSummary = InvestSummary.builder()
+                    .year(year)
+                    .month(month)
+                    .principal(principal)
+                    .build();
+
+            createInvestSummary.changeInvestSetting(em.getReference(InvestSetting.class, investSettingId));
+
+            em.persist(createInvestSummary);
+            em.flush();
+
+            return createInvestSummary;
+        } catch (PersistenceException ex) {
+            incrementPrincipal(investSettingId, year, month, principal);
+            return selectInvestSummary(investSettingId, year, month);
+        }
+    }
+
+    private InvestSummary selectInvestSummary(Long investSettingId, Integer year, Integer month) {
+        InvestSummary is = queryFactory
+                .selectFrom(investSummary)
+                .where(
+                        investSummary.investSetting.id.eq(investSettingId),
+                        investSummary.year.eq(year),
+                        investSummary.month.eq(month)
+                ).fetchOne();
+
+        if (is != null) em.refresh(is);
+        return is;
+    }
+}
